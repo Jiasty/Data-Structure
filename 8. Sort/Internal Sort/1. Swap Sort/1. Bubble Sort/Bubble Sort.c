d84b130054836cd3bca8f0940fd90f773d762ddc
jiasty <2739142322@qq.com>
2024-04-23 16:54:42 +0800

    updata bubble sort

--- 8. Sort/Internal Sort/1. Swap Sort/1. Bubble Sort/Bubble Sort.c
+++ 8. Sort/Internal Sort/1. Swap Sort/1. Bubble Sort/Bubble Sort.c
@@ -6,19 +6,23 @@ void Swap(int* n1, int* n2);
 
 int main()
 {
+	int a[] = {1, 2, 6, 3, 1, 4, 5};
+	BubbleSort(a, sizeof(a) / sizeof(a[0]));
+	for(int i = 0; i < sizeof(a) / sizeof(a[0]);i++)
+		printf("%d ", a[i]);
     return 0;
 }
 
 void BubbleSort(int* a, int n)
 {
-	for (int j = 0; j < n; j++)
+	for (int j = 0; j < n; j++)  // 外层循环控制趟数和影响内层循环每趟的长度
 	{
-		bool exchange = false;
-		for (int i = 1; i < n-j; i++)
+		bool exchange = false;  //可能中间就已经排完了，如果为false就直接跳出循环
+		for (int i = 0; i < n - j - 1; i++)  //每趟循环长度由趟数决定
 		{
-			if (a[i - 1] > a[i])
+			if (a[i] > a[i + 1])
 			{
-				Swap(&a[i - 1], &a[i]);
+				Swap(&a[i], &a[i + 1]);
 				exchange = true;
 			}
 		}
