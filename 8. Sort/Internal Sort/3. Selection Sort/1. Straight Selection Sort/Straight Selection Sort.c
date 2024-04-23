0689deff72c98e7bbd27fa78b82c3330fbc26bb9
jiasty <2739142322@qq.com>
2024-04-23 17:39:40 +0800

    selection sort

---
+++ 8. Sort/Internal Sort/3. Selection Sort/1. Straight Selection Sort/Straight Selection Sort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void SelectionSort1(int* arr, int n);
+void SelectionSort2(int* arr, int n);
+void Swap(int* n1, int* n2);
+
+
+int main()
+{
+    int a[]= {4, 2, 1, 9, 6, 3, 2};
+    SelectionSort1(a, sizeof(a) / sizeof(a[0]));
+    for(int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
+//方法一:从头往后找
+void SelectionSort1(int* arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        Swap(&arr[i], &arr[minIndex]);
+    }
+}
+
+//方法二:前后一起开始找
+void SelectionSort2(int* arr, int n)
+{
+
+}
+
+void Swap(int* n1, int* n2)
+{
+    int tmp = *n1;
+    *n1 = *n2;
+    *n2 = tmp;
+}
