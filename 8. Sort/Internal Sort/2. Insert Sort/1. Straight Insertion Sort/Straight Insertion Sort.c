a6d1456dc46bd9d4d68eed59ba5ceadf88d9df9d
jiasty <2739142322@qq.com>
2024-04-25 13:54:58 +0800

    updata

--- 8. Sort/Internal Sort/2. Insert Sort/1. Straight Insertion Sort/Straight Insertion Sort.c
+++ 8. Sort/Internal Sort/2. Insert Sort/1. Straight Insertion Sort/Straight Insertion Sort.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
-void InsertSort(int* arr, int n);
+void InsertSort1(int* arr, int n);
+void InsertSort2(int* arr, int n);
 void Swap(int* n1, int* n2);
 
 int main()
 {
-    int a[] = {1, 2, 4, 5, 3};
-    InsertSort(a, sizeof(a) / sizeof(a[0]));
+    int a[] = {2, 4, 5, 3, 1};
+    //InsertSort1(a, sizeof(a) / sizeof(a[0]));
+    InsertSort2(a, sizeof(a) / sizeof(a[0]));
     for(int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
     {
         printf("%d ", a[i]);
@@ -15,11 +17,13 @@ int main()
     return 0;
 }
 
-void InsertSort(int* arr, int n)
+//方法一:边比较边交换，利用swap函数而不是挪动数据
+void InsertSort1(int* arr, int n)
 {
     if(n == 1)
         return;
-    // [0, end - 1] end 将 end 位置的元素插入已有区间
+    // [0, end - 1] end 将 end 位置的元素插入有序区间
+    // 最开始的一个元素即使有序
     for(int i = 1; i < n; i++)
     {
         int endi = i;
@@ -30,6 +34,29 @@ void InsertSort(int* arr, int n)
         }
     }
 
+}
+
+//方法二:用下标进行比较，挪动数据赋值覆盖
+void InsertSort2(int* arr, int n)
+{
+    for(int i = 0; i < n - 1; i++)  //注意边界的控制，自行考虑用哪还种
+    {
+        int end = i;
+        int tmp = arr[end + 1];
+        while(end >= 0)
+        {
+            if(arr[end] > tmp)
+            {
+                arr[end + 1] = arr[end];
+                end--;
+            }
+            else
+            {
+                break; //一定注意，tmp > 了arr[end - 1]时要跳出该次循环，否则可能陷入死循环
+            }
+        }
+        arr[end + 1] = tmp;
+    }
 }
 
 void Swap(int* n1, int* n2)
