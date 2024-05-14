66e03d4415cdd43b421ca186a052c3c5df6763e4
jiasty <2739142322@qq.com>
2024-05-14 17:55:21 +0800

    AVL is wrong

--- STL/6.5. Balance Binary Search Tree/AVL Tree/AVL.h
+++ STL/6.5. Balance Binary Search Tree/AVL Tree/AVL.h
@@ -1,3 +1,4 @@
+#pragma once
 #include<iostream>
 #include<assert.h>
 
@@ -38,7 +39,7 @@ struct AVLTNode
     //更新平衡因子是往上走，需要记住parents结点
     pair<K, V> _kv;
 
-    int _bf; //平衡因子(此处为右子树高度 - 左子树高度)
+    int _bf; //balance factor平衡因子(此处为右子树高度 - 左子树高度)
 };
 
 template<class K, class V>
@@ -74,17 +75,17 @@ public:
         }
 
         cur = new Node(kv);
-        if(kv.first < parent->_kv.first)
-        {
-            parent->_left = cur;
-        }
-        else
+        if(kv.first > parent->_kv.first)
         {
             parent->_right = cur;
         }
-        
+        else
+        {
+            parent->_left = cur;
+        }
         //三叉链，链接parent
         cur->_parents = parent;
+
         //更新平衡因子，从下往上更新
         while(parent) //parent为nullptr意味着cur为_root结点，已经不需要更新了
         {
@@ -107,13 +108,31 @@ public:
             else if(parent->_bf == 1 || parent->_bf == -1)
             {
                 //继续往上更新
-                cur = cur->_parents;
+                cur = parent;
                 parent = parent->_parents;
             }
             else if(parent->_bf == 2 || parent->_bf == -2)
             {
                 //该子树已经不平衡了，需要旋转处理！！！！！！
-                // ...
+                if(parent->_bf == 2 && cur->_bf == 1) //左单旋
+                {
+                    RotateLeft(parent);
+                }
+                else if(parent->_bf == -2 && cur->_bf == -1) //右单旋
+                {
+                    RotateRight(parent);
+                }
+                else if(parent->_bf == 2 && cur->_bf == -1) //以下两种都是双旋的情况，不是偏向一边一直高下去
+                {
+                    RotateRL(parent);
+                }
+                else if(parent->_bf == -2 && cur->_bf == 1)
+                {
+                    RotateLR(parent);
+                }
+                
+                //旋转后就意味着平衡因子已经更新好了，不用继续往上更新了，直接break
+                break;
             }
             else
             {
@@ -141,7 +160,7 @@ public:
         return nullptr;
     }
 
-    bool Erase(const pair<K, V>& kv)
+    /* bool Erase(const pair<K, V>& kv)//!!!未完成
     {
         //1、没有孩子:直接删
         //2、有1个孩子:parent托管
@@ -227,32 +246,221 @@ public:
         //没找到，返回false
         return false;
 
-    }
+    } */
 
     //右单旋:左子树太高了
     //左高就让左子树作为新的_root，将自己降为新root的右子树，然后托管新root以前的右子树成为自己的左子树
     void RotateRight(Node* parent)
     {
+        Node* subL = parent->_left;
+        Node* subLR = subL->_right;
 
+
+        parent->_left = subLR;
+        if(subLR)//一定要判断一下subL的右子树是否存在，否则subLR->_parent解引用会报错
+			subLR->_parents = parent;
+
+        subL->_right = parent;
+        Node* p = parent->_parents;
+        parent->_parents = subL;
+
+        if(parent == _root)
+        {
+            _root = subL;
+            _root->_parents = nullptr; //注意此步
+        }
+        else
+        {
+            //parent有父母，判断parent是左孩子还是右孩子
+            if(parent == p->_left)
+            {
+                p->_left = subL;
+            }
+            else
+            {
+                p->_right = subL;
+            }
+
+            subL->_parents = p; //注意要更改_parents指针
+        }
+
+        //底层指针指向转动完成，更改bf
+        subL->_bf = 0;
+        parent->_bf = 0;
     }
     void RotateLeft(Node* parent)
     {
-        
+        Node* subR = parent->_right;
+        Node* subRL = subR->_left;
+
+
+        parent->_left = subRL;
+        if(subRL)//一定要判断一下subL的右子树是否存在，否则subLR->_parent解引用会报错
+			subRL->_parents = parent;
+
+        subR->_left = parent;
+        Node* p = parent->_parents;
+        parent->_parents = subR;
+
+        if(parent == _root)
+        {
+            _root = subR;
+            _root->_parents = nullptr; //注意此步
+        }
+        else
+        {
+            //parent有父母，判断parent是左孩子还是右孩子
+            if(parent == p->_left)
+            {
+                p->_left = subR;
+            }
+            else
+            {
+                p->_right = subR;
+            }
+
+            subR->_parents = p; //注意要更改_parents指针
+        }
+
+        //底层指针指向转动完成，更改bf
+        subR->_bf = 0;
+        parent->_bf = 0;
     }
-    //注意！！！！！！！！！！！！！！！
+
+    //双旋对平衡因子要做额外处理，插入位置不同旋转完因子不同
+    //左右双旋（先左再右）
+    void RotateLR(Node* parent)
+    {
+        Node* subL = parent->_left;
+        Node* subLR = subL->_right;
+
+        //一定在调用单旋前记录原本suLr的bf，不同的插入subL和parent最后的bf不同需单独处理
+        int bf = subLR->_bf;
+
+        RotateLeft(subL);
+        RotateRight(parent);
+
+        subLR->_bf = 0;
+        //单独处理平衡因子
+        if(bf == -1) //插入在subLR的左边
+        {
+            subL->_bf = 0;
+            parent->_bf = 1;
+        }
+        else if(bf == 1)
+        {
+            subL->_bf = -1;
+            parent->_bf = 0;
+        }
+        else if(bf == 0) //此时插入的就是subLR
+        {
+            subL->_bf = 0;
+            parent->_bf = 0;
+        }
+        else
+        {
+            //理论而言不会出现这种情况，为了避免bug，直接将该情况断死报错
+            assert(false);
+        }
+    }
+    //右左双旋
+    void RotateRL(Node* parent)
+    {
+        Node* subR = parent->_right;
+        Node* subRL = subR->_left;
+
+        //一定在调用单旋前记录原本suRl的bf，不同的插入subR和parent最后的bf不同需单独处理
+        int bf = subRL->_bf;
+
+        RotateRight(subR);
+        RotateLeft(parent);
+
+        subRL->_bf = 0;
+        //单独处理平衡因子
+        if (bf == 1)
+		{
+			subR->_bf = 0;
+			parent->_bf = -1;
+		}
+		else if (bf == -1) //插入在subLR的左边
+		{
+			parent->_bf = 0;
+			subR->_bf = 1;
+		}
+		else if(bf == 0)//此时插入的就是subLR
+		{
+			parent->_bf = 0;
+			subR->_bf = 0;
+		}
+        else
+        {
+            //理论而言不会出现这种情况，为了避免bug，直接将该情况断死报错
+            assert(false);
+        }
+    }
+
+
+    //注意封装（避免外部需要_root却无法调用）（递归的时候也封装）！！！！！！！！！！！！！！！
+    //判断是否平衡
+    bool isBalance()
+    {
+        return _isBalance(_root);
+    }
+    int GetHeight()
+    {
+        return _GetHeight(_root);
+    }
     void InOrder()  
     {
         _InOrder(_root);
         cout << endl;
     }
 private:
+    bool _isBalance(Node* root)
+    {
+        //每棵树都判断一下
+
+        if(root == nullptr)
+            return true;
+
+        int leftHeigh = _GetHeight(root->_left);
+        int rightHeigh = _GetHeight(root->_right);
+
+        //此处一定不能判断为正确就返回true，因为不保证下面的树为AVL树
+        if(abs(rightHeigh - leftHeigh) >= 2)
+        {
+            cout << root->_kv.first << endl; //第几次插入就不是
+            return false;
+        }
+
+        //顺便检查平衡因子
+        if(rightHeigh - leftHeigh != root->_bf)
+        {
+            cout << root->_kv.first << endl; //第几个bf错误
+            return false;
+        }
+
+        //递归判断左右子树,同时为true才true
+        return _isBalance(root->_left) && _isBalance(root->_right);
+    }
+    int _GetHeight(Node* root)
+    {
+        if(root == nullptr)
+            return 0;
+        
+        int leftH = _GetHeight(root->_left);
+        int rightH = _GetHeight(root->_right);
+
+        return max(leftH, rightH) + 1;
+    }
+
     void _InOrder(Node* root)  //这里为什么要传root? 外部调用时要传root，但是_root为私有，所以可以再封装
     {
         if(root == nullptr)
             return;
         
         _InOrder(root->_left);
-        cout << root->_kv.first << ":" << root->_kv.second << " " << endl;
+        cout << root->_kv.first << ":" << root->_kv.second << endl;
         _InOrder(root->_right);
     }
 
--- STL/6.5. Balance Binary Search Tree/AVL Tree/Test.cpp
+++ STL/6.5. Balance Binary Search Tree/AVL Tree/Test.cpp
@@ -2,15 +2,25 @@
 
 void AVL_Test()
 {
-    int a[] = {8, 3, 10, 1, 6};
+    int a[] = { 8, 3, 1, 10, 6 };
+    //{ 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+    //int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
 
     AVLTree<int, int> At;
     for(auto& e : a)
     {
+        //调试用
+        // if(e == 4)
+        // {
+        //     int i = 0;
+        // }
         At.Insert({e, e});
+        //cout << At.isBalance() << endl;
     }
 
+    cout << "-------" << endl;
     At.InOrder();
+    cout << At.isBalance() << endl;
 
 }
 
