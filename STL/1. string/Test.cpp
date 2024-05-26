2584c31a8d7817ed6b96c43285331f493ad298a0
jiasty <2739142322@qq.com>
2024-05-26 14:26:59 +0800

    string

--- STL/1. string/Test.cpp
+++ STL/1. string/Test.cpp
@@ -2,7 +2,83 @@
 
 using namespace Jiastystring;
 
+void test1()
+{
+    Jiastystring::string str1("You are my apple of eyes");
+    Jiastystring::string:: iterator it = str1.begin();
+    while(it != str1.end())
+    {
+        cout << *it++ << " ";
+    }
+    cout << endl;
+
+    for(auto e : str1)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+
+    const Jiastystring::string str2("hello world");
+    Jiastystring::string:: const_iterator cit = str2.begin();
+    while(cit != str2.end())
+    {
+        //(*cit)++; 不可修改
+        cout << *cit++ << " ";
+    }
+    cout << endl;
+
+    for(auto ch : str2)
+    {
+        cout << ch << " ";
+    }
+    cout << endl;
+    
+}
+
+void test2()
+{
+    Jiastystring::string str1("hello world");
+    str1.push_back('1');
+    str1.push_back('2');
+    str1.append("125715");
+    str1 += '0';
+    str1 += "1111";
+    cout << str1.c_str() << endl;
+    for(auto ch : str1)
+    {
+        cout << ch << " ";
+    }
+    cout << endl;
+}
+
+void test3()
+{
+    //insert and erase
+    Jiastystring::string str1("hello world");
+    str1.insert(5, 'x');
+    cout << str1.c_str() << endl;
+    str1.insert(0, 'x');
+    cout << str1.c_str() << endl;
+
+    str1.erase(5, 3);
+    cout << str1.c_str() << endl;
+
+    str1.resize(20, 'x');
+    cout << str1.c_str() << endl;
+}
+
+void test4()
+{
+    Jiastystring::string str1("hello world");
+
+    Jiastystring::string str2 = str1;//不深拷贝就会在析构时释放两次
+}
+
 int main()
 {
+    //test1();
+    //test2();
+    //test3();
+    test4();
     return 0;
 }
--- STL/1. string/string.h
+++ STL/1. string/string.h
@@ -9,6 +9,27 @@ namespace Jiastystring
     class string
     {
     public:
+        //迭代器(顺序存储就适合原生指针)
+        typedef char* iterator;
+        typedef const char* const_iterator;
+        iterator begin()
+        {
+            return _str;
+        }
+        iterator end()
+        {
+            return _str + _size;
+        }
+        const_iterator begin() const
+        {
+            return _str;
+        }
+        const_iterator end() const
+        {
+            return _str + _size;
+        }
+
+        //默认成员函数
         // string()
         //     :_str(new char[1]) //注意string是有\0的
         //     ,_size(0)
@@ -35,6 +56,26 @@ namespace Jiastystring
             strcpy(_str, str);
         }
 
+        string(const string& s)
+        {
+            _str = new char[s._capacity + 1];
+            strcpy(_str, s._str);
+            _size = s._size;
+            _capacity = s._capacity;
+        }
+
+        string& operator=(const string& s)
+        {
+            char* tmp = new char[s._capacity + 1];
+            strcpy(tmp, s._str);
+            delete[] _str;
+            _str = tmp;
+            _size = s._size;
+            _capacity = s._capacity;
+
+            return *this;
+        }
+
         ~string()
         {
             delete[] _str;
@@ -47,10 +88,16 @@ namespace Jiastystring
             return _str;
         }
 
+
         //遍历
         size_t size() const
         {
             return _size;
+        }
+
+        size_t capacity() const
+        {
+            return _capacity;
         }
 
         char& operator[](size_t pos)
@@ -67,9 +114,120 @@ namespace Jiastystring
 
             return _str[pos];
         }
+
+        //Modify
+        void resize(size_t n, char ch = '\0')
+        {
+            if(n <= _size)
+            {
+                _str[n] = '\0';
+            }
+            else
+            {
+                reserve(n);
+                for(size_t i = _size; i < n; i++)
+                {
+                    _str[i] = ch;
+                }
+                _str[n] = '\0';
+                _size = n;
+            }
+        }
+
+        void reserve(size_t n)
+        {
+            if(n > _capacity)  //n > _capacity时才扩容
+            {
+                char* tmp = new char[n + 1]; //永远提前为\0开一个空间
+                strcpy(tmp, _str);
+                delete[] _str;
+                _str = tmp;
+                _capacity = n;
+            }
+        }
+
+        void push_back(char ch)
+        {
+            //扩容2倍
+            if(_capacity == _size)
+            {
+                reserve(_capacity == 0 ? 4 : 2 * _capacity);
+            }
+            //插入
+            _str[_size] = ch;
+            ++_size;
+            _str[_size] = '\0';
+        }
+
+        void append(const char* str)
+        {
+            //扩容到需要的空间
+            size_t len = strlen(str);
+            if(_size + len > _capacity)
+            {
+                reserve(_size + len);
+            }
+            strcpy(_str + _size, str);//strcpy会将\0给拷贝下来
+            _size += len;
+        }
+
+        string& operator+=(char ch)
+        {
+            push_back(ch);
+            return *this;
+        }
+        string& operator+=(const char* str)
+        {
+            append(str);
+            return *this;
+        }
+
+        void insert(size_t pos, char ch)
+        {
+            assert(pos <= _size);
+            //扩容2倍
+            if(_capacity == _size)
+            {
+                reserve(_capacity == 0 ? 4 : 2 * _capacity);
+            }
+            //挪动数据 !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            size_t end = _size + 1; //即使将end改为intleixing也会发生整型提升出错,所以还得强转一下pos为int.
+            while(end > pos)  //要么改end = _size+1,让循环结束条件不是end<0时结束
+            {
+                _str[end] = _str[end - 1];
+                --end; //end为size_t,不能小于0,所以当pos == 0时就不会结束循环
+            }
+            _str[pos] = ch;
+            ++_size;//勿忘
+        }
+        void insert(size_t pos, const char* str)
+        {
+            
+        }
+
+        void erase(size_t pos, size_t len = npos) //在pos位置删除len个字符
+        {
+            assert(pos < _size); //不能删\0
+
+            if(len == npos || len >= _size - pos) //len + pos >= _size 会有溢出风险当len很大很大时
+            {
+                _str[pos] = '\0';
+                _size = pos;
+            }
+            else
+            {
+                strcpy(_str + pos, _str + pos + len);
+                _size -= len;
+            }
+        }
+
     private:
         char* _str;
         size_t _size;
         size_t _capacity;
+    public:
+        static const int npos;
     };
+
+    const int string::npos = -1; //不加static
 }
